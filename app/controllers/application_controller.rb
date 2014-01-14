0b0460262d09eaa30daecbfcc4e0579cb9bb0df4
Simon Taranto <simon.taranto@gmail.com>
2014-01-14 13:24:36 -0700

    implement port 9292 for all requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :use_port_9292
 
   protect_from_forgery with: :exception
-  
+
+  def use_port_9292
+    class << request
+      def port
+        9292
+      end
+    end
+  end
+
 end
