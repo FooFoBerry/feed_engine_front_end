c2ff7fe0785fbc84c64e1f985500faf25da5e33f
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-16 11:05:52 -0700

    add relative asset config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :url_for
+
   def current_user_id
     @user_id ||= cookies[:user_id]
   end
-
 end
