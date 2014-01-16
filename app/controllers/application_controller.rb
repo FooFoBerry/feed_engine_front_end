16ecd56a409a821fa7be14809b913deb95ce4cfc
Simon Taranto <simon.taranto@gmail.com>
2014-01-15 18:12:16 -0700

    remove 9292 port redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :use_port_9292
-
   protect_from_forgery with: :exception
-
-  def use_port_9292
-    class << request
-      def port
-        9292
-      end
-    end
-  end
 
 end
