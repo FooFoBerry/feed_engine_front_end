13032b5c7e48dfccd1df507e0e14ea89a09f8a22
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-21 15:22:04 -0700

    use backup non-minified assets if compiled assets can't be found

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ FeedEngineFrontEnd::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true ## FALL BACK
 
   # Generate digests for assets URLs.
   config.assets.digest = true
